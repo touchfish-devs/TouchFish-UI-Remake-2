5b3cecdc97eaee2da40ff9fe75ad05bdfc97a06a
Piaoztsdy <Piaoztsdy@outlook.com>
2026-08-13 00:01:40 +0800

    [fix] 修复引用 bug

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     authRequest: (payload) => ipcRenderer.invoke('auth-request', payload),
